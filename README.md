2e3e55130a5cf851b152338d78059918bfecb5eb
ffanizza <ffanizza@hawk.iit.edu>
2018-01-20 14:26:16 -0500

    added more pictures, added post requests

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SpartanHacksCarControl
 Toggle components on a car
+
+Image from kbb:
+https://www.kbb.com/car-pictures/2016-tesla-model-x-suv-pictures/
--- scripts/file.js
+++ scripts/file.js
@@ -1,147 +1,110 @@
 window.onload = function() {
     var isFrunkOpen = false;
     var isFLDoorOpen = false;
-    var isFLWindOpen = false;
-    var isFRWindOpen = false;
     var isFRDoorOpen = false;
     var isBLDoorOpen = false;
-    var isBLWindOpen = false;
-    var isBRWindOpen = false;
     var isBRDoorOpen = false;
     var isTrunkOpen = false;
+    var isWindowsOpen = false;
     document.getElementById("frunk").onclick = function(oClick) {
-        // open frunk
         if (!isFrunkOpen) {
-            // call post open frunk
-            isFrunkOpen = true;
-            oClick.path[0].src = "img/frunkopen.png";
-            r = requests.post('http://hackathon.intrepidcs.com/api/data', headers={'Authorization': 'Bearer '}, form={ 'command': 'openFrontDriverDoor' }).text
+            if (postRequest("frunk")) {
+                isFrunkOpen = true;
+                oClick.path[0].src = "img/frunkopen.png";
+            }
         }
     };
     document.getElementById("flDoor").onclick = function(oClick) {
-        // toggle flDoor
         if (!isFLDoorOpen) {
-            // call post open fldoor
-            oClick.path[0].src = "img/fldooropen.png";
-            oClick.path[0].alt = "Close front left door";
+            if (postRequest("openFrontDriverDoor")) {
+                oClick.path[0].src = "img/fldooropen.png";
+                oClick.path[0].alt = "Close front left door";
+                isFLDoorOpen = !isFLDoorOpen;
+            }
         } else {
-            // call post close fldoor
-            oClick.path[0].src = "img/fldoorclosed.png";
-            oClick.path[0].alt = "Open front left door";
+            if (postRequest("closeFrontDriverDoor")) {
+                oClick.path[0].src = "img/fldoorclosed.png";
+                oClick.path[0].alt = "Open front left door";
+                isFLDoorOpen = !isFLDoorOpen;
+            }
         }
-        isFLDoorOpen = !isFLDoorOpen;
-    };
-    document.getElementById("flWind").onclick = function(oClick) {
-        // toggle flWind
-        if (!isFLWindOpen) {
-            // call post open flwind
-            oClick.path[0].src = "img/flwindopen.png";
-            oClick.path[0].alt = "Close front left window";
-        } else {
-            // call post close flwind
-            oClick.path[0].src = "img/flwindclosed.png";
-            oClick.path[0].alt = "Open front left window";
-        }
-        isFLWindOpen = !isFLWindOpen;
-    };
-    document.getElementById("frWind").onclick = function(oClick) {
-        // toggle frWind
-        if (!isFRWindOpen) {
-            // call post open frwind
-            oClick.path[0].src = "img/frwindopen.png";
-            oClick.path[0].alt = "Close front right window";
-        } else {
-            // call post close frwind
-            oClick.path[0].src = "img/frwindclosed.png";
-            oClick.path[0].alt = "Open front right window";
-        }
-        isFRWindOpen = !isFRWindOpen;
     };
     document.getElementById("frDoor").onclick = function(oClick) {
-        // toggle frDoor
         if (!isFRDoorOpen) {
-            // call post open fldoor
-            oClick.path[0].src = "img/frdooropen.png";
-            oClick.path[0].alt = "Close front right door";
+            if (postRequest("openFrontPassengerDoor")) {
+                oClick.path[0].src = "img/frdooropen.png";
+                oClick.path[0].alt = "Close front right door";
+                isFRDoorOpen = !isFRDoorOpen;
+            }
         } else {
-            // call post close fldoor
-            oClick.path[0].src = "img/frdoorclosed.png";
-            oClick.path[0].alt = "Open front right door";
+            if (postRequest("closeFrontPassengerDoor")) {
+                oClick.path[0].src = "img/frdoorclosed.png";
+                oClick.path[0].alt = "Open front right door";
+                isFRDoorOpen = !isFRDoorOpen;
+            }
         }
-        isFRDoorOpen = !isFRDoorOpen;
     };
     document.getElementById("blDoor").onclick = function(oClick) {
-        // toggle blDoor
         if (!isBLDoorOpen) {
-            // call post open fldoor
-            oClick.path[0].src = "img/bldooropen.png";
-            oClick.path[0].alt = "Close back left door";
+            if (postRequest("openBackDriverDoor")) {
+                oClick.path[0].src = "img/bldooropen.png";
+                oClick.path[0].alt = "Close back left door";
+                isBLDoorOpen = !isBLDoorOpen;
+            }
         } else {
-            // call post close fldoor
-            oClick.path[0].src = "img/bldoorclosed.png";
-            oClick.path[0].alt = "Open back left door";
+            if (postRequest("closeBackDriverDoor")) {
+                oClick.path[0].src = "img/bldoorclosed.png";
+                oClick.path[0].alt = "Open back left door";
+                isBLDoorOpen = !isBLDoorOpen;
+            }
         }
-        isBLDoorOpen = !isBLDoorOpen;
-    };
-    document.getElementById("blWind").onclick = function(oClick) {
-        // toggle blWind
-        if (!isBLWindOpen) {
-            // call post open blwind
-            oClick.path[0].src = "img/blwindopen.png";
-            oClick.path[0].alt = "Close back left window";
-        } else {
-            // call post close blwind
-            oClick.path[0].src = "img/blwindclosed.png";
-            oClick.path[0].alt = "Open back left window";
-        }
-        isBLWindOpen = !isBLWindOpen;
-    };
-    document.getElementById("brWind").onclick = function(oClick) {
-        // toggle brWind
-        if (!isBRWindOpen) {
-            // call post open brwind
-            oClick.path[0].src = "img/brwindopen.png";
-            oClick.path[0].alt = "Close back right window";
-        } else {
-            // call post close fldoor
-            oClick.path[0].src = "img/brwindclosed.png";
-            oClick.path[0].alt = "Open back right window";
-        }
-        isBRWindOpen = !isBRWindOpen;
     };
     document.getElementById("brDoor").onclick = function(oClick) {
-        // toggle brDoor
         if (!isBRDoorOpen) {
-            // call post open fldoor
-            oClick.path[0].src = "img/brdooropen.png";
-            oClick.path[0].alt = "Close back right door";
+            if (postRequest("openBackPassengerDoor")) {
+                oClick.path[0].src = "img/brdooropen.png";
+                oClick.path[0].alt = "Close back right door";
+                isBRDoorOpen = !isBRDoorOpen;
+            }
         } else {
-            // call post close fldoor
-            oClick.path[0].src = "img/brdoorclosed.png";
-            oClick.path[0].alt = "Open back right door";
+            if (postRequest("closeBackPassengerDoor")) {
+                oClick.path[0].src = "img/brdoorclosed.png";
+                oClick.path[0].alt = "Open back right door";
+                isBRDoorOpen = !isBRDoorOpen;
+            }
         }
-        isBRDoorOpen = !isBRDoorOpen;
+    };
+    document.getElementById("windows").onclick = function(oClick) {
+        if (!isWindowsOpen) {
+            if (postRequest("openAllWindows")) {
+                isWindowsOpen = !isWindowsOpen;
+                oClick.path[0].src = "img/windowsopen.png";
+            }
+        } else {
+            if (postRequest("closeAllWindows")) {
+                isWindowsOpen = !isWindowsOpen;
+                oClick.path[0].src = "img/windowsclosed.png";
+            }
+        }
     };
     document.getElementById("trunk").onclick = function(oClick) {
-        // open trunk
         if (!isTrunkOpen) {
-            // call post open trunk
-            isTrunkOpen = true;
-            oClick.path[0].src = "img/trunkopen.png";
+            if (postRequest("trunk")) {
+                isTrunkOpen = true;
+                oClick.path[0].src = "img/trunkopen.png";
+            }
         }
     };
+    // Generic post request
+    postRequest = function(command) {
+        var response = fetch(
+            'http://hackathon.intrepidcs.com/api/data', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer 6c299d424e90cd865af81922cc397d586c8785d2f2368608a8cf0fa4edd6d407'
+            },
+            body: {'command': command}
+        });
+        return command === response.data.command;
+    };
 }
-
-
-/*
-$.ajax({
-    url: “www.example.com/information.php”,
-    data: { page: 2 },
-    method: “post”,
-    success: function(result) {
-       $(“#someElement”).html(result);
-    },
-    error: function(result) {
-       alert(“Ajax call failed!”);
-    }
- }); */
